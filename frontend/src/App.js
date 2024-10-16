886a3d3e93825de0fe9cbd3c84395319d189d439
devik-nangia <nangiadevik@gmail.com>
2024-10-16 10:02:47 +0530

    navbar hero and match center

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useRef } from 'react'
+import Navbar from './Navbar.js'
+import Hero from './Hero.js'
+import MatchCenter from './MatchCenter.js'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
+const App = () => {
+  const matchCenterRef = useRef(null);
+
+  // Function to scroll to MatchCenter
+  const scrollToMatchCenter = () => {
+    if (matchCenterRef.current) {
+      matchCenterRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
   return (
-    <div className="text-3xl font-bold underline">
-      <p>Hello World</p>
-    </div>
-  );
+    <Router>
+      <main className='bg-gradient-to-r from-background-dark to-[#1b202c]'>
+        <Navbar scrollToMatchCenter={scrollToMatchCenter} />
+        <Routes>
+          <Route path="/" element={
+            <div>
+              <Hero scrollToMatchCenter={scrollToMatchCenter} />
+              <div ref={matchCenterRef}>
+                <MatchCenter />
+              </div>
+            </div>
+          } />
+        </Routes>
+      </main>
+    </Router>
+  )
 }
 
-export default App;
+export default App
+
